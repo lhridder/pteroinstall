559c28f13bfde42e74568d95df2b8ff29d07af72
lhridder <63594063+lhridder@users.noreply.github.com>
2021-04-21 14:54:38 +0200

    fixed autodeploy command and incorrect nginx config

--- pteroinstall.sh
+++ pteroinstall.sh
@@ -4,7 +4,7 @@ output(){
     echo -e '\e[36m'$1'\e[0m';
 }
 
-version=v1.1
+version=v1.2
 
 importssh(){
 apt install -y ssh-import-id
@@ -27,7 +27,7 @@ installdaemon() {
 
     output "Add node on panel, server ip: $SERVER_IP"
     read choice
-    ${choice}
+    eval $choice
 
     output "Put daemon into systemd"
     cat > /etc/systemd/system/wings.service <<- 'EOF'
@@ -148,18 +148,18 @@ set_real_ip_from 103.21.244.0/22;
 set_real_ip_from 103.22.200.0/22;
 set_real_ip_from 103.31.4.0/22;
 set_real_ip_from 104.16.0.0/12;
-set_real_ip_from 104.16.0.0/13
-set_real_ip_from 104.24.0.0/14
-set_real_ip_from 108.162.192.0/18
-set_real_ip_from 131.0.72.0/22
-set_real_ip_from 141.101.64.0/18
-set_real_ip_from 162.158.0.0/15
-set_real_ip_from 172.64.0.0/13
-set_real_ip_from 173.245.48.0/20
-set_real_ip_from 188.114.96.0/20
-set_real_ip_from 190.93.240.0/20
-set_real_ip_from 197.234.240.0/22
-set_real_ip_from 198.41.128.0/17
+set_real_ip_from 104.16.0.0/13;
+set_real_ip_from 104.24.0.0/14;
+set_real_ip_from 108.162.192.0/18;
+set_real_ip_from 131.0.72.0/22;
+set_real_ip_from 141.101.64.0/18;
+set_real_ip_from 162.158.0.0/15;
+set_real_ip_from 172.64.0.0/13;
+set_real_ip_from 173.245.48.0/20;
+set_real_ip_from 188.114.96.0/20;
+set_real_ip_from 190.93.240.0/20;
+set_real_ip_from 197.234.240.0/22;
+set_real_ip_from 198.41.128.0/17;
 set_real_ip_from 2400:cb00::/32;
 set_real_ip_from 2606:4700::/32;
 set_real_ip_from 2803:f800::/32;
@@ -243,6 +243,7 @@ ssl() {
         output "The entered domain does not resolve to the primary public IP of this server."
         output "Please make an A record pointing to your server's IP. For example, if you make an A record called 'panel' pointing to your server's IP, your FQDN is panel.domain.tld"
         output "If you are using Cloudflare, please disable the orange cloud."
+        exit 0
     else
         output "Domain resolved correctly. Good to go..."
     fi
